ad96f864076ad600420a3dcba195494946f6e873
William Porto <william.porto@nave.rs>
2021-08-19 09:37:23 -0300

    chore(app): initial commit

---
+++ android/app/src/main/kotlin/com/powilliam/bs_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.powilliam.bs_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:bs_app/src/app.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
