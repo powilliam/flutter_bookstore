6522b47bc90d4c2b62e16f72cdb2df8585a8c892
William Porto <william.porto@nave.rs>
2021-08-24 10:11:40 -0300

    feat(app): changing status bar and navigation bar colors

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,20 @@ import 'package:bs_app/src/viewmodels/bookmarks_viewmodel.dart';
 import 'package:bs_app/src/viewmodels/books_viewmodel.dart';
 import 'package:bs_app/src/viewmodels/shoppingcart_viewmodel.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+    statusBarColor: Colors.white,
+    statusBarBrightness: Brightness.light,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(
         create: (_) => BooksViewModel(AppContainer.provideBooksRepository())
